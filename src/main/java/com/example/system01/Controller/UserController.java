da6a86b21535c571590432d4792fa55a80358efe
jianggq <1459601242@qq.com>
2023-07-26 16:51:29 +0800

    4

--- src/main/java/com/example/system01/Controller/UserController.java
+++ src/main/java/com/example/system01/Controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.system01.Controller;
 import com.example.system01.Dto.User;
 import com.example.system01.Mapper.UserMapper;
 import com.example.system01.Service.UserService;
+import com.example.system01.utils.JwtUtil;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,19 +30,34 @@ public class UserController {
     @PostMapping("/login")
     public String login(@RequestBody User user, HttpServletResponse response) {
 
+        String Token = null;
+
         //对用户输入的密码进行md5转换
         String md5password = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
 
         //调用service服务查询数据库是否存在当前用户
         User dbUser = userService.getUserByNameAndPassword(user.getUsername(), md5password);
         if (dbUser != null) {
-            return "qiutian";
+            //创建Token并返回给前端
+            Token = JwtUtil.createToken(user.getId(), user.getUsername());
+            System.out.println(Token);
+            return Token;
         }
         return "wrong";
     }
 
     /**
+     * 检测请求头里的token
+     */
+    @GetMapping("check_token")
+    public boolean checkToken(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        return JwtUtil.checkToken(token);
+    }
+
+    /**
      * 查询所有用户信息
+     *
      * @return 查询成功则返回List<User>,否则返回null
      */
     @PostMapping("/getAll")
@@ -58,6 +75,7 @@ public class UserController {
 
     /**
      * 添加一名用户
+     *
      * @param user
      * @return
      */
